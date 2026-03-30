f945627bdbc2c19c587f1b45dbb17b3f74a56064
Mossy Jimmerson <74316073+MossMom@users.noreply.github.com>
2026-03-30 13:49:22 -0600

    Fill files

---
+++ Group26/about.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet">
+
+        <title>
+            [] About Page
+        </title>
+
+        <meta charset="UTF-8">
+        <meta name="description" content="T26's About Page">
+        <meta name="keywords" content="Color Picker About">
+        <meta name="author" content="Mossy Jimmerson, Jack Birlingmair, Elijah Gillit">
+    </head>
+
+   <body>
+        <div class="boxConstraint">
+            <header>
+                <div id="banner" class="window">
+                    <h1>[] Color Picker</h1>
+                </div>
+                <br>
+                <div id="navBar">
+                    <nav>
+                        <a href="index.php">Home</a>
+                        <a href="about.php">About</a>
+                        <a href="color.php">Color Coordinator</a>
+                    </nav>
+                </div>
+                
+            </header>
+
+            <div>
+                <table cellspacing="20px" id="containerTable">
+                    <tr>
+                        <td class="mainContent">
+                            <div class="window">
+                                <h1>~~~~~~~~~~~ Test ~~~~~~~~~~~</h1>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <footer>
+                <div class="footer">
+                    <h5>Webpage made by Mossy, Jack, & Elijah</h5>
+                </div>
+            </footer>
+        </div>    
+    </body>
+</html>
